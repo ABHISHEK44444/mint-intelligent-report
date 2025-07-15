3fa490d4b027706b04d5bbba7b4efd9e01afcee2
ABHISHEK44444 <153176102+ABHISHEK44444@users.noreply.github.com>
2025-07-15 15:22:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://mint-intelligent-report.onrender.com',
+        target: 'http://localhost:5001',
         changeOrigin: true,
       }
     }
